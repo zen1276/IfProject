566f1be7e90b99f39cb4edfaa440d6ad3e8ee038
Van hoffman <zvan1276@740s-pl-119.local>
2018-09-20 08:27:11 -0600

    added javadoc to the controller

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -1,7 +1,22 @@
 package controller;
 
+//--------- Import Section --------
+import model.IfProperties;
+import javax.swing.JOptionPane;
+
 public class IfController
 {
+	/**
+	 * Builds the instance of the controller
+	 */
+	public IfController()
+	{
+		
+	}
+	
+	/**
+	 * This is where the program starts calling methods to run the program
+	 */
 	public void start()
 	{
 		
