9e47f0613b18a230e40d2d859bbc75a390817099
Van hoffman <zvan1276@740s-pl-119.local>
2018-09-25 08:50:33 -0600

    added validINT and validDouble

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -47,9 +47,54 @@ public class IfController
 			compNumber++;
 		}
 		
-		
-		
-		
-		
+		for (int loop = 0; loop < 30; loop += 2)
+		{
+			JOptionPane.showInternalMessageDialog(null, "The loop value is: " + loop);
+		}	
 	}
+	
+	private void askUser()
+	{
+		String response = JOptionPane.showInputDialog, "How many hours??";
+		
+		while (!validDouble(response))
+		{
+			
+		}
+	}
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "Please type a valid\ninteger value");
+		}
+		
+		return isValid;
+	}
+	
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "This requires a double value");
+		}
+		
+		return isValid;
+	}
+	
 }
